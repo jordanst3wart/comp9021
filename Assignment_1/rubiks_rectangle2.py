2c3d6810a3bfdac4c22dae0691f51b1c61b839e1
jordan stewart <jordanstewart2428@gmail.com>
2016-04-10 14:47:45 +1000

    got old file

--- Assignment_1/rubiks_rectangle2.py
+++ Assignment_1/rubiks_rectangle2.py
@@ -5,11 +5,9 @@
 
 import sys
 import re
-from collections import deque
+import math
 
-# TODO make faster
-# I might use collection.deque to deque the closest one
-# I might check three nodes at a time
+
 def calculate_number_of_steps(rectangle):
     step = 0
     step_position = 8
@@ -17,47 +15,18 @@ def calculate_number_of_steps(rectangle):
     while True:
         pattern=patterns[0]
         print(pattern)
-
-        # spawn three permutations each pattern
-        patterns.append(row_exchange(pattern))
-        patterns.append(right_shift(pattern))
-        patterns.append(middle_clockwise_rotation(pattern))
         if ( match_pattern(pattern, rectangle) ):
             return pattern[step_position]
-
-
-def calculate_number_of_steps_deque(rectangle):
-    step = 0
-    step_position = 8
-    patterns = deque([['1','2','3','4','5','6','7','8', step]])
-    while True:
-        print(patterns[0])
-        if match_pattern_deque(patterns[0], rectangle):
-                return patterns[0][step_position]
-
-        print(patterns[0])
-        # spawn three permutations each pattern
-        patterns.append(row_exchange(patterns[0]))
-        patterns.append(right_shift(patterns[0]))
-        patterns.append(middle_clockwise_rotation(patterns[0]))
-        #pattern = patterns.popleft()
-        print(patterns[0])
-        patterns.popleft()
-        print(patterns[0])
-        #print(pattern)
-
-
-
-def match_pattern_deque(pattern, rectangle):
-    if pattern != rectangle:
-        return False
-    return True
-
+        else:
+            # spawn three permutations each pattern
+            patterns.append(right_shift(pattern))
+            patterns.append(middle_clockwise_rotation(pattern))
+            del patterns[0]
 
 
 def match_pattern(pattern, rectangle):
     # delete step variable in list
-    temp = pattern
+    temp = pattern.pop()
     if pattern == rectangle:
         pattern.append(temp)
         return True
@@ -71,7 +40,7 @@ def row_exchange(pattern):
     # actually [8,7,6,5,4,3,2,1] or as input 87654321
     order=[7,6,5,4,3,2,1,0,8]
     pattern = [ pattern[j] for j in order]
-    pattern=add_step(pattern[8])
+    pattern=add_step(pattern)
     return pattern
 
 
@@ -79,7 +48,7 @@ def right_shift(pattern):
     # actually [4,1,2,3,6,7,8,5] or as input 41236785
     order=[3,0,1,2,5,6,7,4,8]
     pattern = [ pattern[j] for j in order]
-    pattern=add_step(pattern[8])
+    pattern=add_step(pattern)
     return pattern
 
 
@@ -87,13 +56,13 @@ def middle_clockwise_rotation(pattern):
     # actually [1,7,2,4,5,3,6,8] or as input 17245368
     order = [0,6,1,3,4,2,5,7,8]
     pattern = [ pattern[j] for j in order]
-    pattern = add_step(pattern[8])
+    pattern = add_step(pattern)
     return pattern
 
 
-def add_step(step):
-    step += 1
-    return step
+def add_step(pattern):
+    pattern[8] += 1
+    return pattern
 
 
 try:
@@ -103,15 +72,10 @@ try:
     # TODO I should check if the values input are numbers
     if len(line)!=8:
         raise ValueError
-    for i in line:
-        int(i)      # gives up if not an int
-    if not(''.join(sorted(line)) == '12345678'):
-        raise ValueError
 except ValueError:
     print('Incorrect input, giving up...')
     sys.exit()
 
-#value = calculate_number_of_steps_deque(line)
 value = calculate_number_of_steps(line)
 print(value, 'steps are needed to reach the final configuration.')
 # collections.deque nah
