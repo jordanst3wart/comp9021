d3bc8208ae7733107246e7d4d78697ee7a8db21a
jordan stewart <jordanstewart2428@gmail.com>
2016-04-30 21:52:11 +1000

    add specific test file and updated tangram file

--- Assignment_2/tangram.py
+++ Assignment_2/tangram.py
@@ -2,6 +2,7 @@
 # Worse is better
 
 import re
+import math
 
 # 1. is input valid
 # 2. two files are identical
@@ -22,29 +23,74 @@ class Tangram():
 
 # return true if valid pieces
 def are_valid(coloured_pieces):
+    for piece in coloured_pieces:
+        if not(piece.check_angle()) or not(piece.check_sides()):
+            return False
     return True
 
-
+# return pieces ( a list of pieces), a piece is a list of points
 def available_coloured_pieces(file):
     # I would like to map
-    pieces=list(map(parse_xml,file))
-    return pieces # should return pieces
+    return list(map(parse_xml,file))
+
 
 # input could be file but the file should be iterateable
-# returns pieces
+# returns piece, a list of points
 def parse_xml(line):
+    if "svg" in line:
+        pass    # might return something weird
+    piece = []
     line=re.split('\"',line)
     line=line[1].split()
-    # M is the start, x,y L, and z is the end
+    for string in line:
+        if string:
+            pass
+        else:
+            piece.append(Point(string,string.next)) # might not remove from que
+    return piece
 
 
+class Point:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
 
-# classes??
-# point??
-# line??
-# piece??
+    # get true bearing angle from two points
+    def get_angle(self,point):
+        # get gradient
+        # TODO do maths for this part
+        angle = math.atan2((point.y - self.y),(point.x - self.x))
+        angle = 90 - angle
+        return angle
 
 
+# a group of points
+class Piece():
+    def __init__(self,point_list):
+        self.point_list = point_list
+
+    # convex and clockwise angles
+    def check_angle(self):
+        angles = []
+        size = len(self.point_list)
+        i=0
+        while i <= size:
+            if i == size: # last
+                angles.append(self.point_list[i].get_angle(self.point_list[0]))
+            else:
+                angles.append(self.point_list[i].get_angle(self.point_list[i+1]))
+
+        # get a list of true bearings
+        # check if ascending or descending
+        # substraction previous angle
+        # all should be below 180
+        return True
+
+    def check_sides(self):
+        if len(self.point_list)<3:
+            return False
+        else:
+            return True
 
 
 
